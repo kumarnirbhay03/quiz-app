bf60efdc99a7e54f525cf3fdbbe5ce3f2c8a8896
kumarnirbhay03 <kumarnirbhayrsvm@gmail.com>
2022-06-30 11:51:54 +0530

    modified

--- src/questions.js
+++ src/questions.js
@@ -4,14 +4,14 @@ const questions=[
         choices:["Mumbai Indians","Chennai Superkings","Kolkata Night Riders","Sunrisers Hyderabad"],
         correct: 1,
         desc: "Mumbai won in 2013,2015,2017,2019,2020",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR28Ymp07phKq3Dov2bDGNk1Zz3tXXDYVV1-g&usqp=CAU",
+        image: "",
     },
     {
         question:'Which team won ipl 4 times?',
         choices:["Mumbai Indians","Chennai Superkings","Kolkata Night Riders","Sunrisers Hyderabad"],
         correct: 2,
         desc: "Chennai won in 2010,2011,2018,2021",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPdNF6TroUPFOpQK73vydWcAs56U51eMVCg&usqp=CAU",
+        image: "https://images.indianexpress.com/2020/09/ipl-schedule.jpg",
     },
     {
         question:'In which year india won world cup',
@@ -21,25 +21,18 @@ const questions=[
         image: "None",
     },
     {
-        question:'Which team won ipl 4 times',
-        choices:["Mumbai Indians","Chennai Superkings"],
-        correct: 1,
-        desc: "won in 2018",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPdNF6TroUPFOpQK73vydWcAs56U51eMVCg&usqp=CAU",
+        question:'Full form of ATM?',
+        choices:["Automatic teller machine ","Automated teller machine","Automated terailer machine"],
+        correct: 2,
+        desc: "Automated teller machine",
+        image: "https://st.adda247.com/https://s3-ap-south-1.amazonaws.com/adda247jobs-wp-assets-adda247/jobs/wp-content/uploads/sites/2/2021/08/19174453/atm-full-form.jpg",
     },
     {
-        question:'Which team won ipl 4 times',
-        choices:["Mumbai Indians","Chennai Superkings"],
-        correct: 1,
-        desc: "won in 2018",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPdNF6TroUPFOpQK73vydWcAs56U51eMVCg&usqp=CAU",
-    },
-    {
-        question:'Which team won ipl 4 times',
-        choices:["Mumbai Indians","Chennai Superkings"],
-        correct: 1,
-        desc: "won in 2018",
-        image: "None",
+        question:'Which is the heighest mountain peak on earth?',
+        choices:["Karakoram","Lhotse","Makalu","Mount Everest"],
+        correct: 4,
+        desc: "Mount Everest(8849m)",
+        image: "https://cdn.britannica.com/17/83817-050-67C814CD/Mount-Everest.jpg",
     }
 ];
 export default questions;
